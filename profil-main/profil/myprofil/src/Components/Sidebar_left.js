e6a6313679a1555ef59953bc2f22aff68a55be8f
YuraOstrik <134145610+YuraOstrik@users.noreply.github.com>
2025-07-09 20:45:38 +0300

    Add files via upload

---
+++ profil-main/profil/myprofil/src/Components/Sidebar_left.js
@@ -0,0 +1,14 @@
+import './Sidebar_left.css'
+
+export function Sidebar_left(){
+
+    return(
+        <aside className='sidebar_left'>
+            
+        </aside>
+    );
+}
+
+
+
+
---
+++ profil-main/profil/myprofil/src/Components/Sidebar_right.js
@@ -0,0 +1,38 @@
+import './Sidebar_right.css'
+
+export function Sidebar_right(){
+
+    return(
+        <aside className='sidebar_right'>
+
+        </aside>
+    );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
